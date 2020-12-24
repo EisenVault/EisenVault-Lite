cd9b687f18ee3348409a324d8434c30506044aad
ShayaneB <shayane.basu@eisenvault.com>
2020-12-24 14:26:06 +0530

    Improved tests

--- src/test/testUtils.js
+++
@@ -1,9 +0,0 @@
-/**
- * Return node(s) with the given data-test attribute.
- * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper
- * @param {string} val - Value of data-test attribute for search
- */
-
-export const findByTestAttr = (wrapper, val) => {
-    return wrapper.find(`[data-test="${val}"]`)
-}
